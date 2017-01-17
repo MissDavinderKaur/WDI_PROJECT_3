3c195bfd1a6bfde5875a9f1d37852ed44e07ded9
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2017-01-17 17:02:20 +0000

    date format is finally correct

--- makeMyNight/models/plan.js
+++ makeMyNight/models/plan.js
@@ -1,10 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose   = require('mongoose');
+const dateformat = require('dateformat');
 
 const planSchema = new mongoose.Schema({
   name: { type: String },
-  date: { type: Date },
+  date: { type: Date, get: v => dateformat(v, 'formattedDate') },
   attendees: { type: Number },
   bookings: [{ type: mongoose.Schema.ObjectId, ref: 'Booking' }]
+}, {
+  toJSON: {
+    getters: true
+  }
 });
+
+dateformat.masks.formattedDate = 'dddd dS mmmm, yyyy';
 
 module.exports = mongoose.model('Plan', planSchema);
--- makeMyNight/src/js/app.js
+++ makeMyNight/src/js/app.js
@@ -120,195 +120,196 @@ Planner.login = function(e) {
 
     Planner.showLoggedInUser = function(user) {
       Planner.ajaxRequest(`${Planner.apiURL}/users/${user._id}`, 'GET', `${user._id}`, user => {
-        Planner.$main.html(`<div class="content"> <h2>Hello... ${user.name}</h2>
-          <img src="../images/profilePic.jpeg" alt="Davinder Kaur">
-          <h5> <b> Email: </b> ${user.emailAddress}</h5>
-          <h5> <b> Phone: </b> ${user.mobile}</h5>
-          <br>
-          <br>
-          <span class="col-md-12">
-          <form class="newPlanForm" method="post" action="/users/${user._id}/plans">
-          <div class="form-group col-md-3">
-          <input class="form-control" type="text" name="plan[name]" placeholder="Plan Name" required>
-          </div>
-          <div class="form-group col-md-3">
-          <input class="form-control" type="number" name="plan[attendees]" placeholder="No. of Attendees" required>
-          </div>
-          <div class="form-group col-md-3">
-          <button class="btn btn-primary"> Make a new Night Plan! </button>
-          </div>
-          </form>
-          </span>
-          <h5> My Previous Plans</h5> </div>`);
-          for( var i = 0; i < user.plans.length; i++) {
-            Planner.$main.append(`<div class="content"> <h6> <a href="${Planner.apiURL}/users/${user._id}/plans/${user.plans[i]._id}" class="planDetail"> ${user.plans[i].name} </a> on ${user.plans[i].date} </h6> </div>`);
-          }
+        Planner.$main.html(`<div class="content">
+        <img src="../images/profilePic.jpeg" alt="Davinder Kaur">
+        <h2>Hello....... ${user.name}</h2>
+        <h6> <b> Email: </b> ${user.emailAddress}</h6>
+        <h6> <b> Phone: </b> ${user.mobile}</h6>
+        <br>
+        <br>
+        <span class="col-md-12 newPlanSpan">
+        <form class="newPlanForm" method="post" action="/users/${user._id}/plans">
+        <div class="form-group col-md-3">
+        <input class="form-control" type="text" name="plan[name]" placeholder="Plan Name" required>
+        </div>
+        <div class="form-group col-md-3">
+        <input class="form-control" type="number" name="plan[attendees]" placeholder="No. of Attendees" required>
+        </div>
+        <div class="form-group col-md-3">
+        <button class="btn btn-primary"> Make a new Night Plan! </button>
+        </div>
+        </form>
+        </span>
+        <h5> My Previous Plans</h5> </div>`);
+        for( var i = 0; i < user.plans.length; i++) {
+          Planner.$main.append(`<div class="content"> <h6 class="previousPlans"> <a href="${Planner.apiURL}/users/${user._id}/plans/${user.plans[i]._id}" class="planDetail"> ${user.plans[i].name} </a> on ${user.plans[i].date} </h6> </div>`);
+        }
       });
     };
 
-      Planner.showPlanDetail = function(e) {
-        e.preventDefault();
-        return Planner.ajaxRequest(e.currentTarget.href, 'GET', null, plan => {
-          console.log(plan);
-          Planner.$main.html(`<div class="content"> <a href="/"> Back to my page </a>
-          <h6> ${plan.name} (${plan.attendees} people) on ${plan.date} </h6> </div>`);
-          for( var i = 0; i < plan.bookings.length; i++) {
-            Planner.$main.append(`<div class="content"> <h6> ${plan.bookings[i].description}</h6> </div>`);
-          }
-        });
+    Planner.showPlanDetail = function(e) {
+      e.preventDefault();
+      return Planner.ajaxRequest(e.currentTarget.href, 'GET', null, plan => {
+        console.log(plan);
+        Planner.$main.html(`<div class="content"> <a href="/"> Back to my page </a>
+        <h6> ${plan.name} (${plan.attendees} people) on ${plan.date} </h6> </div>`);
+        for( var i = 0; i < plan.bookings.length; i++) {
+          Planner.$main.append(`<div class="content"> <h6> ${plan.bookings[i].description}</h6> </div>`);
+        }
+      });
+    };
+
+
+    Planner.newPlan = function(e) {
+      e.preventDefault();
+      console.log('clicked');
+      Planner.ajaxRequest(`${Planner.apiURL}${$(this).attr('action')}`, 'POST', $(this).serialize(), plan => {
+        Planner.currentPlan = plan;
+      });
+      Planner.loadMap();
+      Planner.addTheatres();
+    };
+
+    Planner.loadMap = function() {
+      Planner.$main.html(`<div id="map-canvas"></div>`);
+      const canvas = document.getElementById('map-canvas');
+
+      const mapOptions = {
+        zoom: 13,
+        center: new google.maps.LatLng(51.5154,-0.1410),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
       };
 
+      this.map = new google.maps.Map(canvas, mapOptions);
+    };
 
-      Planner.newPlan = function(e) {
-        e.preventDefault();
-        console.log('clicked');
-        Planner.ajaxRequest(`${Planner.apiURL}${$(this).attr('action')}`, 'POST', $(this).serialize(), plan => {
-          Planner.currentPlan = plan;
-        });
-        Planner.loadMap();
-        Planner.addTheatres();
-      };
-
-      Planner.loadMap = function() {
-        Planner.$main.html(`<div id="map-canvas"></div>`);
-        const canvas = document.getElementById('map-canvas');
-
-        const mapOptions = {
-          zoom: 13,
-          center: new google.maps.LatLng(51.5154,-0.1410),
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-
-        this.map = new google.maps.Map(canvas, mapOptions);
-      };
-
-      Planner.addTheatres = function() {
-        return Planner.ajaxRequest(`${Planner.apiURL}/venues`, 'GET', null, theatres => {
-          $.each(theatres, (index, theatre) => {
-            let events = '';
-            $.each(theatre.events, (index, event) => {
-              events += `<li> <a href="#" class="choosePerformance" data-id="${event.EventId}"> ${event.Name} </a> </li>`;
+    Planner.addTheatres = function() {
+      return Planner.ajaxRequest(`${Planner.apiURL}/venues`, 'GET', null, theatres => {
+        $.each(theatres, (index, theatre) => {
+          let events = '';
+          $.each(theatre.events, (index, event) => {
+            events += `<li> <a href="#" class="choosePerformance" data-id="${event.EventId}"> ${event.Name} </a> </li>`;
+          });
+          setTimeout(function() {
+            const latlng = new google.maps.LatLng(theatre.latitude, theatre.longitude);
+            const marker = new google.maps.Marker({
+              position: latlng,
+              map: Planner.map,
+              animation: google.maps.Animation.DROP
             });
-            setTimeout(function() {
-              const latlng = new google.maps.LatLng(theatre.latitude, theatre.longitude);
-              const marker = new google.maps.Marker({
-                position: latlng,
-                map: Planner.map,
-                animation: google.maps.Animation.DROP
+            google.maps.event.addListener(marker, 'click', () => {
+              if (typeof this.infoWindow !== 'undefined') this.infoWindow.close();
+              this.infoWindow = new google.maps.InfoWindow({
+                content: `<p>${ theatre.name } : <br> <ul> ${events} </ul> </p>`
               });
-              google.maps.event.addListener(marker, 'click', () => {
-                if (typeof this.infoWindow !== 'undefined') this.infoWindow.close();
-                this.infoWindow = new google.maps.InfoWindow({
-                  content: `<p>${ theatre.name } : <br> <ul> ${events} </ul> </p>`
-                });
-                this.infoWindow.open(Planner.map, marker);
-                Planner.map.setCenter(marker.getPosition());
-                Planner.map.setZoom(15);
-              });
-            }, index * 50);
-          });
+              this.infoWindow.open(Planner.map, marker);
+              Planner.map.setCenter(marker.getPosition());
+              Planner.map.setZoom(15);
+            });
+          }, index * 50);
         });
-      };
+      });
+    };
 
-      Planner.choosePerformance = function(e) {
-        e.preventDefault();
-        Planner.tempBookingShowName = e.target.innerHTML;
-        Planner.ajaxRequest(`${Planner.apiURL}/Events/${$(this).data('id')}/Performances`, 'GET', null, data => {
+    Planner.choosePerformance = function(e) {
+      e.preventDefault();
+      Planner.tempBookingShowName = e.target.innerHTML;
+      Planner.ajaxRequest(`${Planner.apiURL}/Events/${$(this).data('id')}/Performances`, 'GET', null, data => {
 
-          const filteredPerformances = (data.Performances).filter(p => p.TotalAvailableTickesCount > Planner.currentPlan.attendees);
+        const filteredPerformances = (data.Performances).filter(p => p.TotalAvailableTickesCount > Planner.currentPlan.attendees);
 
-          Planner.$main.html(` <div class="content"> <h2> ${Planner.currentPlan.name} : <br>
-            ${Planner.currentPlan.attendees} tickets for ${Planner.tempBookingShowName} </h2>
-            <p> ${filteredPerformances.length} options available </p>
-            <p> Got a budget? Search by max ticket price </p>
-            <form class="budgetSearch" data-id=${data.EventId}> <input class="form-control userBudget" type="number" name="userBudget" required> <button class="btn btn-primary"> Search </button> </form> </div>`);
+        Planner.$main.html(` <div class="content"> <h2> ${Planner.currentPlan.name} : <br>
+        ${Planner.currentPlan.attendees} tickets for ${Planner.tempBookingShowName} </h2>
+        <p> ${filteredPerformances.length} options available </p>
+        <p> Got a budget? Search by max ticket price </p>
+        <form class="budgetSearch" data-id=${data.EventId}> <input class="form-control userBudget" type="number" name="userBudget" required> <button class="btn btn-primary"> Search </button> </form> </div>`);
 
-            for (var i = 0; i < filteredPerformances.length; i++) {
-              Planner.$main.append(`<div class="content"><h6> ${data.Performances[i].PerformanceDate} <button class="bookShow" data-id="${data.Performances[i].PerformanceDate}"> Book </button> </h6> </div>`);
-            }
-          });
-        };
+        for (var i = 0; i < filteredPerformances.length; i++) {
+          Planner.$main.append(`<div class="content"><h6> ${data.Performances[i].PerformanceDate} <button class="bookShow" data-id="${data.Performances[i].PerformanceDate}"> Book </button> </h6> </div>`);
+        }
+      });
+    };
 
-        Planner.choosePerformanceByBudget = function(e) {
-          e.preventDefault();
-          Planner.ajaxRequest(`${Planner.apiURL}/Events/${$(this).data('id')}/Performances`, 'GET', null, data => {
+    Planner.choosePerformanceByBudget = function(e) {
+      e.preventDefault();
+      Planner.ajaxRequest(`${Planner.apiURL}/Events/${$(this).data('id')}/Performances`, 'GET', null, data => {
 
-            const filteredPerformances = (data.Performances).filter(p => p.MinimumTicketPrice < ($(e.target).find('.userBudget').val()));
+        const filteredPerformances = (data.Performances).filter(p => p.MinimumTicketPrice < ($(e.target).find('.userBudget').val()));
 
-            Planner.$main.html(` <div class="content"> <h2> ${Planner.currentPlan.name} : <br>
-              ${Planner.currentPlan.attendees} tickets for ${Planner.tempBookingShowName} (max £ ${$(e.target).find('.userBudget').val()} per ticket)</h2>
-              <p> ${filteredPerformances.length} options available </p>
-              <button data-id=${data.EventId} class="choosePerformance"> Back to full search </button> </div> `);
-              for (var i = 0; i < filteredPerformances.length; i++) {
-                Planner.$main.append(`<div class="content"> <h6> ${data.Performances[i].PerformanceDate} <button class="bookShow" data-id="${data.Performances[i].PerformanceDate}"> Book </button> </h6> </div>`);
+        Planner.$main.html(` <div class="content"> <h2> ${Planner.currentPlan.name} : <br>
+        ${Planner.currentPlan.attendees} tickets for ${Planner.tempBookingShowName} (max £ ${$(e.target).find('.userBudget').val()} per ticket)</h2>
+        <p> ${filteredPerformances.length} options available </p>
+        <button data-id=${data.EventId} class="choosePerformance"> Back to full search </button> </div> `);
+        for (var i = 0; i < filteredPerformances.length; i++) {
+          Planner.$main.append(`<div class="content"> <h6> ${data.Performances[i].PerformanceDate} <button class="bookShow" data-id="${data.Performances[i].PerformanceDate}"> Book </button> </h6> </div>`);
+        }
+      });
+    };
+
+    Planner.bookShow = function(e) {
+      e.preventDefault();
+      Planner.currentPlan.date = ($('.bookShow').attr('data-id'));
+      const data = Planner.currentPlan
+
+      Planner.ajaxRequest(
+        `${Planner.apiURL}/users/${Planner.loggedInUserID}/plans/${Planner.currentPlan._id}`, 'PUT',
+        data,
+        plan => {
+          Planner.currentPlan = plan;
+
+          const booking = {
+            type: 'Show',
+            description: Planner.tempBookingShowName
+          };
+
+          Planner.ajaxRequest(
+            `${Planner.apiURL}/users/${Planner.loggedInUserID}/plans/${Planner.currentPlan._id}`,
+            'POST',
+            booking,
+            plan => {
+              console.log(plan);
+              Planner.$main.html(`<a href="/"> Back to my page </a>
+              <h6> ${plan.name} (${plan.attendees} people) on ${plan.date} </h6>`);
+              for( var i = 0; i < plan.bookings.length; i++) {
+                Planner.$main.append(`<div class="content"> <h6> ${plan.bookings[i].type}: ${plan.bookings[i].description} </h6> </div>`);
               }
             });
-          };
+          });
+        };
 
-          Planner.bookShow = function(e) {
-            e.preventDefault();
-            Planner.currentPlan.date = ($('.bookShow').attr('data-id'));
-            const data = Planner.currentPlan
-
-            Planner.ajaxRequest(
-              `${Planner.apiURL}/users/${Planner.loggedInUserID}/plans/${Planner.currentPlan._id}`, 'PUT',
-              data,
-              plan => {
-                Planner.currentPlan = plan;
-
-                const booking = {
-                  type: 'Show',
-                  description: Planner.tempBookingShowName
-                };
-
-                Planner.ajaxRequest(
-                  `${Planner.apiURL}/users/${Planner.loggedInUserID}/plans/${Planner.currentPlan._id}`,
-                  'POST',
-                  booking,
-                  plan => {
-                    console.log(plan);
-                    Planner.$main.html(`<a href="/"> Back to my page </a>
-                    <h6> ${plan.name} (${plan.attendees} people) on ${plan.date} </h6>`);
-                    for( var i = 0; i < plan.bookings.length; i++) {
-                      Planner.$main.append(`<div class="content"> <h6> ${plan.bookings[i].type}: ${plan.bookings[i].description} </h6> </div>`);
-                    }
-                  });
-              });
-          };
-
-              Planner.logout = function(e) {
-                e.preventDefault();
-                Planner.generateWelcomePage();
-                Planner.loggedOutState();
-                return window.localStorage.clear();
-              };
+        Planner.logout = function(e) {
+          e.preventDefault();
+          Planner.generateWelcomePage();
+          Planner.loggedOutState();
+          return window.localStorage.clear();
+        };
 
 
-              //INTERNAL HELPER FUNCTIONS
-              Planner.getToken = function(){
-                return window.localStorage.getItem('token');
-              };
+        //INTERNAL HELPER FUNCTIONS
+        Planner.getToken = function(){
+          return window.localStorage.getItem('token');
+        };
 
-              Planner.ajaxRequest = function(url, method, data, callback){
-                return $.ajax({
-                  url,
-                  method,
-                  data,
-                  beforeSend: this.setRequestHeader.bind(this)
-                })
-                .done(callback)
-                .fail(data => {
-                  console.log(data);
-                });
-              };
+        Planner.ajaxRequest = function(url, method, data, callback){
+          return $.ajax({
+            url,
+            method,
+            data,
+            beforeSend: this.setRequestHeader.bind(this)
+          })
+          .done(callback)
+          .fail(data => {
+            console.log(data);
+          });
+        };
 
-              Planner.setToken = function(token){
-                return window.localStorage.setItem('token', token);
-              };
+        Planner.setToken = function(token){
+          return window.localStorage.setItem('token', token);
+        };
 
-              Planner.setRequestHeader = function(xhr) {
-                return xhr.setRequestHeader('Authorization', `Bearer ${this.getToken()}`);
-              };
+        Planner.setRequestHeader = function(xhr) {
+          return xhr.setRequestHeader('Authorization', `Bearer ${this.getToken()}`);
+        };
 
 
-              $(Planner.init.bind(Planner));
+        $(Planner.init.bind(Planner));
